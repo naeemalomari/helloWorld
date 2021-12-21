e84da419194baf640cc69fb21ea347128671fe96
naeemalomari <naeemalomari96@gmail.com>
2021-12-21 20:24:13 +0200

    adding spome features

---
+++ src/main/java/com/autocare/demo/student/StudentController.java
@@ -0,0 +1,23 @@
+package com.autocare.demo.student;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+
+    @RequestMapping
+    public List<Student> getAllStudents() {
+        return List.of(
+                new Student(UUID.randomUUID(),
+                        "Naim", "AlOmari",
+                        "naeemalomari@gmail.com", Student.Gender.MALE),
+                new Student(UUID.randomUUID(), "Slim", "Shady",
+                        "slimshady@gmail.com", Student.Gender.FEMALE)
+        );
+    }
+}
