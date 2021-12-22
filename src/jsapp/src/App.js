87b88d18da04e724d5b1fe3451d65ee54027245d
naeemalomari <naeemalomari96@gmail.com>
2021-12-22 22:10:25 +0200

    adding

--- src/jsapp/src/App.js
+++ src/jsapp/src/App.js
@@ -2,31 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 import { getAllStudents } from './client';
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+
+  app.use(
+    '/students', //this is your api
+    createProxyMiddleware({
+      target: 'http://localhost:8080/students', //this is your whole endpoint link
+      changeOrigin: true,
+    })
+  );
+}
 class App extends Component {
 
-//   state = {
-//     students: []
-//   }
-// componentDidMount () {
-//   this.fetchStudents();
-// }
-  // fetchStudents = () => {
-  //   getAllStudents()
-  //   .then(response => response.json()
-  //   .then(students => {
-  //     console.log(students);
-      // this.setState({ students });
-    // }));
-  // }
-  render() {
-    var stringified = JSON.stringify(data);
-var parsedObj = JSON.parse(stringified);
+  state = {
+    students: []
+  }
+  componentDidMount() {
+    this.fetchStudents();
+  }
+
+  fetchStudents = () => {
     getAllStudents()
-    .then(response => response.json()
-    .then(students => {
-      console.log(students);
-      // this.setState({ students });
-    }));
+      .then(response => response.json()
+        .then(students => {
+          console.log(students);
+          this.setState({ students });
+        }));
+  }
+
+  render() {
+    getAllStudents()
+      .then(response => response.json()
+        .then(students => {
+          console.log(students);
+          this.setState({ students });
+        }));
     return <h1> hello my name is slim shady</h1>
   }
 }
